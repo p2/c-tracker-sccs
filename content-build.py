a9de0a221b0c926319f9fb94ebd33c691a4f734e
Pascal Pfiffner <phase.of.matter@gmail.com>
2017-03-22 11:51:38 +0100

    Update content-build script

--- content-build.py
+++ content-build.py
@@ -1,23 +1,16 @@
 #!/usr/bin/env python3
 #
-#  Uses HTML data from c-tracker-sccs-content and renders them, using Jinja2, into CTracker/HTMLContent/
+#  Uses Markdown and HTML data from _source and renders them, using Jinja2, into _target
 #
 #  Requirements:
 #  - jinja2
 #  - markdown
 
-import io
-import glob
-import os.path
-import markdown
-from jinja2 import Environment, PackageLoader
-
-
-env = Environment(loader=PackageLoader(__name__, 'content-templates'))
-app_template = env.get_template('App.html')
-dir_source = 'c-tracker-sccs-content'
-dir_target = 'App'
-files = [
+_template_dir = 'content-templates'
+_template_name = 'App.html'
+_source = 'c-tracker-sccs-content'
+_target = 'App'
+_files = [
 	'PrivacyPolicy.md',
 	'LicenseAgreement.md',
 	'AboutHepC.md',
@@ -28,61 +21,7 @@ files = [
 	'WhoCanParticipate.md'
 ]
 
-def lang_name(langpath):
-	return os.path.splitext(os.path.split(langpath)[-1])[0]
+if '__main__' == __name__:
+	exec(open('c-tracker-sccs-content/build.py').read())
+	run(_template_dir, _template_name, _source, _target, _files)
 
-def file_content(langpath, filename):
-	filepath = os.path.join(langpath, filename)
-	
-	# not found in desired language, fall back to en
-	if not os.path.exists(filepath):
-		print('~~~>  «{}» does not exist in {}, trying English for'.format(filename, lang_name(langpath)))
-		altpath = os.path.join(dir_source, 'en.lproj')
-		filepath = os.path.join(altpath, filename)
-		if not os.path.exists(filepath):
-			print('xxx>  «{}» not found, skipping'.format(file))
-			return None, None, None
-	
-	# read file
-	with io.open(filepath, 'r', encoding="utf-8") as handle:
-		content = handle.read()
-		title = os.path.splitext(filename)[0]
-		
-		# markdown?
-		if '.md' == os.path.splitext(filename)[-1]:
-			filename = os.path.splitext(filename)[0] + '.html'
-			title = content.split('\n')[0]
-			content = markdown.markdown(content, output_format='html5')
-	
-	return filename, title, content
-
-for langpath in glob.iglob(os.path.join(dir_source, '*.lproj')):
-	print('->  Language {}'.format(lang_name(langpath)))
-	langdir = os.path.split(langpath)[-1]
-	
-	for file in files:
-		filename = None
-		title = None
-		content = []
-		
-		# read file(s): expecting either a string (one file) or a tuple with a
-		# list of files (0) and the target file name (1)
-		subfiles = [file]
-		if isinstance(file, tuple):
-			subfiles = file[0]
-			filename = file[1]
-		
-		for subfile in subfiles:
-			myname, mytitle, mycontent = file_content(langpath, subfile)
-			if filename is None:
-				filename = myname
-			if title is None:
-				title = mytitle
-			if mycontent is not None:
-				content.append(mycontent)
-		if 0 == len(content):
-			continue
-		
-		# render
-		app_template.stream(title=title, content='\n\n'.join(content)) \
-			.dump(os.path.join(dir_target, langdir, filename))
